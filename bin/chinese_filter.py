e22c36fedaf9942039c3aeff28ddda59f4c3cf05
Ceres <543457946@qq.com>
2020-08-15 20:48:08 +0800

    update

--- bin/chinese_filter.py
+++ bin/chinese_filter.py
@@ -8,17 +8,19 @@ __current_folder__ = os.path.dirname(__file__)
 chinses_charas_path = os.path.join(__current_folder__, "../data/chinese_characters.txt")
 chinese_charas = [line.strip() for line in open(chinses_charas_path, 'r', encoding='utf-8').readlines()]
 __conventer__ = opencc.OpenCC('t2s.json')
+punctuation = zhong_punc + string.punctuation
 
 
 def validate_label(label):
-    label = __conventer__.convert(label)
     return_label = ""
     for chara in label:
         if chara in chinese_charas:
             return_label += chara
         else:
-            if chara in zhong_punc or chara in string.punctuation:
+            if chara in punctuation:
                 return_label += ""
+            elif __conventer__.convert(chara) in punctuation:
+                return_label += __conventer__.convert(chara)
             else:
                 return None
     return return_label
