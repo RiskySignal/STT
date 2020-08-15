5b9302771c2838a7f017277655cb37de395f2bba
Ceres <543457946@qq.com>
2020-08-15 22:33:52 +0800

    update

--- bin/chinese_filter.py
+++ bin/chinese_filter.py
@@ -1,26 +1,34 @@
 # coding=utf-8
 import os
-from zhon.hanzi import punctuation as zhong_punc
 import string
-import opencc
+
+from pypinyin import pinyin, Style
+from zhconv import convert
+from zhon.hanzi import punctuation as zhong_punc
 
 __current_folder__ = os.path.dirname(__file__)
 chinses_charas_path = os.path.join(__current_folder__, "../data/chinese_characters.txt")
 chinese_charas = [line.strip() for line in open(chinses_charas_path, 'r', encoding='utf-8').readlines()]
-__conventer__ = opencc.OpenCC('t2s.json')
-punctuation = zhong_punc + string.punctuation
+punctuation = zhong_punc + string.punctuation + " ・─"
+
+tmp_set = set()
 
 
 def validate_label(label):
     return_label = ""
+    label = convert(label, "zh-cn")
     for chara in label:
         if chara in chinese_charas:
             return_label += chara
         else:
             if chara in punctuation:
                 return_label += ""
-            elif __conventer__.convert(chara) in punctuation:
-                return_label += __conventer__.convert(chara)
             else:
-                return None
+                if pinyin(chara, style=Style.TONE3, heteronym=False)[0][0] == chara:
+                    return None
+                else:
+                    if chara not in tmp_set:
+                        print(chara, pinyin(chara, style=Style.TONE3, heteronym=False))
+                        tmp_set.add(chara)
+                    return None
     return return_label
